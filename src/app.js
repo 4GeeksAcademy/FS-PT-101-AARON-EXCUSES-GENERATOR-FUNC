496524e774dbd1cab9209dea87cd40effd4cbc75
aaron2210 <64928964+aaron2210@users.noreply.github.com>
2025-03-17 20:02:48 +0000

    fead:generador de excusas con funciones

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let textoExcusa = document.getElementById("excuseGenerada");
+textoExcusa.innerHTML=excuseResult(who, action, what, when);
+
+  console.log(textoExcusa.innerHTML);
 };
+const randomNumber = (array) => Math.floor(Math.random()*array.length); // return i del array;
+const randomEle = (array) => array[randomNumber(array)]; //return el valor dentro del array;
+const excuseResult = (array1, array2, array3, array4) => `${randomEle(array1)} ${randomEle(array2)} ${randomEle(array3)} ${randomEle(array4)}`;
+
+
+
